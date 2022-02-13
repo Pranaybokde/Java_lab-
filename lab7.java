8388dd4afaedfb8d8e31adebcadc487fa739021f
Pranaybokde <90241539+Pranaybokde@users.noreply.github.com>
2022-02-13 19:08:44 +0530

    Update lab7.java

--- lab7.java
+++ lab7.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class lab7
-{                         //Program by Pravesh Tiwari
+{                         //Program by Pranay Bokde
       public static void main(String[] args)
     {
        
