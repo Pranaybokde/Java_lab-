1b4ba0d223f25f7c30e876bc3d86282f7a0ecb44
Pranaybokde <90241539+Pranaybokde@users.noreply.github.com>
2022-02-13 18:13:20 +0530

    Create lab2.java

---
+++ lab2.java
@@ -0,0 +1,10 @@
+ public class lab2{              
+                     //Program by Pravesh Tiwari
+     public static void main(String args[])
+    {
+       
+        System.out.print("Hello World ");
+    }
+    }
+© 2022 GitHub, Inc.
+Terms
