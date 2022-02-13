0e54e07d0db5a424eaf62ff349345a239834846e
Pranaybokde <90241539+Pranaybokde@users.noreply.github.com>
2022-02-13 19:13:00 +0530

    Update lab15.java

--- lab15.java
+++ lab15.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class lab15
-{                        //Program by Pravesh Tiwari
+{                        //Program by Pranay Bokde
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
